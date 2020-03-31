030b55254bf67bbe175c439b827b050cfadff3d2
Marissa Conner <marissafromtexas@gmail.com>
2020-03-30 20:00:58 -0700

    Pseudoclassical revision

--- src/init.js
+++ src/init.js
@@ -18,9 +18,10 @@ $(document).ready(function() {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window[dancerMakerFunctionName];//this holds blinky's constructor
 
     // make a dancer with a random position
+
 
     var dancer = dancerMakerFunction(
       $("body").height() * Math.random(),
